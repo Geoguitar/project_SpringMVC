8700cf04407b86510fc7d125545e5ddf76b25a18
Geovane Silveira <geovanedsilveira@gmail.com>
2023-07-16 12:29:36 -0300

    Atualizado para esta branch

--- dslist-backend/src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
+++ dslist-backend/src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
@@ -1,7 +1,6 @@
 package com.devsuperior.dslist.dto;
 
 import com.devsuperior.dslist.entities.Game;
-import com.devsuperior.dslist.projections.GameMinProjection;
 
 public class GameMinDTO {
 
@@ -17,14 +16,6 @@ public class GameMinDTO {
 		year = entity.getYear();
 		imgUrl = entity.getImgUrl();
 		shortDescription = entity.getShortDescription();
-	}
-
-	public GameMinDTO(GameMinProjection projection) {
-		id = projection.getId();
-		title = projection.getTitle();
-		year = projection.getGameYear();
-		imgUrl = projection.getImgUrl();
-		shortDescription = projection.getShortDescription();
 	}
 
 	public Long getId() {
